a83a86c864eddceb4fc79b4f973d7e9f20280c2f
Robin_ <3045351695@qq.com>
2024-08-03 19:38:35 +0800

    "前端工程初始化"

---
+++ user-center-backend/src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.usercenter.mapper;
+
+import com.yupi.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 久居白昼
+* @description 针对表【user(用户表)】的数据库操作Mapper
+* @createDate 2024-06-16 20:27:13
+* @Entity com.yupi.usercenter.model.domain.User.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ user-center-backend/src/test/java/com/yupi/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,24 @@
+package com.yupi.usercenter;
+
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void myTest(){
+        System.out.println("hello");
+    }
+    
+
+
+}
